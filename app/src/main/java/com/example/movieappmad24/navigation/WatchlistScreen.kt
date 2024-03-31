98369851df25b0b79b9ad35cf6840c93c5d2cd2f
fkukcha <ferdaws_kukcha@yahoo.com>
2024-03-31 18:07:36 +0200

    Refactored.

--- app/src/main/java/com/example/movieappmad24/navigation/WatchlistScreen.kt
+++ app/src/main/java/com/example/movieappmad24/navigation/WatchlistScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
 import com.example.movieappmad24.MovieRow
 import com.example.movieappmad24.TopAppBar
 import com.example.movieappmad24.models.Movie
@@ -12,10 +11,8 @@ import com.example.movieappmad24.models.getMovies
 
 @Composable
 fun WatchlistScreen(onMovieClick: (Movie) -> Unit) {
-    val navController = rememberNavController()
-
     Column {
-        TopAppBar(title = "Watchlist")
+        TopAppBar(title = "Your Watchlist")
         LazyColumn {
             items(getMovies()) { movie ->
                 MovieRow(movie = movie, onMovieClick = onMovieClick)
